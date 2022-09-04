b18991699dd092f59394d09284e0451a8c0edd3d
zmmohibul <18-36427-1@student.aiub.edu>
2022-09-05 01:05:40 +0600

    Understanding State

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 const el = document.getElementById("root");
 
@@ -8,6 +10,30 @@ const root = ReactDOM.createRoot(el);
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { lat: null, errorMessage: "" };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  renderContent() {
+    if (this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    } else if (this.state.errorMessage) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    } else {
+      return <Spinner message="Please accept location request" />;
+    }
+  }
+
+  render() {
+    return <div>{this.renderContent()}</div>;
   }
 }
 
